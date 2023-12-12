5cb511e9aee1c8950345df190268bb0908d99f97
s33805 <s33805@hotmail.com>
2023-12-12 20:14:42 +0800

    商品功能 - Refactor category，使用 Enum 類型

---
+++ src/main/java/com/markting/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.markting.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- src/main/java/com/markting/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/markting/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,6 +1,7 @@
 package com.markting.springbootmall.rowmapper;
 import java.util.Date;
 
+import com.markting.springbootmall.constant.ProductCategory;
 import com.markting.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,7 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
