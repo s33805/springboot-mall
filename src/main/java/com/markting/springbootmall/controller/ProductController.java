8ebc0ae3f07b411e774fd333882e10e8c7765233
s33805 <s33805@hotmail.com>
2023-12-13 10:18:16 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/markting/springbootmall/controller/ProductController.java
+++ src/main/java/com/markting/springbootmall/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.markting.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +58,12 @@ public class ProductController {
         Product updateProduct = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
